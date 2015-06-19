b1c58989fa8bb60f50d8f2b55ff071878491ab8e
Ben Cornelis <bencornelis02@gmail.com>
2015-06-19 12:57:39 -0700

    bind mini-nav selected class to isSelected nav item attribute

--- app/components/mini-nav.js
+++ app/components/mini-nav.js
@@ -2,10 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    click: function(item) {
-      $("#mini-nav li").removeClass('selected')
-      $("#" + item.route).addClass('selected');
-      this.get('currentController').transitionToRoute(item.route);
+    click: function(clickedItem) {
+      // $("#mini-nav li").removeClass('selected')
+      // $("#" + item.route).addClass('selected');
+      this.get('items').forEach(function(item) {
+        item.set('isSelected', false);
+      })
+      clickedItem.set('isSelected', true);
+      this.get('currentController').transitionToRoute(clickedItem.route);
     }
   }
 });
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,17 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   navItems: [
-    {
+    Ember.Object.create({
       route: 'questions',
-      name: 'Questions'
-    },
-    {
+      name: 'Questions',
+      isSelected: true
+    }),
+    Ember.Object.create({
       route: 'tags',
       name: 'Tags'
-    },
-    {
+    }),
+    Ember.Object.create({
       route: 'new-question',
       name: 'Add Question'
-    }
+    })
   ]
 });
