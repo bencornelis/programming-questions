9a167d44bbf721380ffc03f10685a3ea4eb80fcd
Ben Cornelis <bencornelis02@gmail.com>
2015-06-19 17:31:41 -0700

    fix answer author bug by adding author to answer model

--- app/models/answer.js
+++ app/models/answer.js
@@ -5,5 +5,6 @@ export default DS.Model.extend({
   accepted: DS.attr('boolean'),
   votes: DS.attr('number'),
   timestamp: DS.attr('date'),
+  author: DS.attr('string'),
   question: DS.belongsTo('question', {async: true})
 });
